bde1609ac5a1ff9c3beede59b9bfe5693b309a54
jim <jim@jimb.cc>
2018-04-18 13:50:32 +0100

    created array of documents containing ingredients fixed some typos

--- sarnies.rb
+++ sarnies.rb
@@ -21,7 +21,7 @@ Mongo::Logger.logger.level = ::Logger::WARN
 # An Array of cities
 @bread = ["White Bread", "Brown Bread", "a Seeded bun", "a kiln fired flatbread", "a pitta", "a wrap", "sourdough", "a croissant", "rye bread", "a artisan baked stone ground bloomer", "a barm cake", "a traditional northern cob"]
 @price = [1.99, 2.99, 3.99, 3.49, 3.75, 3.99, 4.49, 4.99, 5.49, 5.99, 8.99]
-@range = ["Basic", "Kids", "Xtra Hungry", "Artisan", "Home Made", "Luxurary", "Bespoke"]
+@range = ["Basic", "Kids", "Xtra Hungry", "Artisan", "Home Made", "Luxury", "Bespoke"]
 # Insert some documents into the database
 def insertDocs(coll)
 
@@ -31,12 +31,30 @@ def insertDocs(coll)
   # Insert this many documents
   for i in 1..ARGV[0].to_i
 
+    ing1 = Faker::Food.ingredient
+    ing2 = Faker::Food.ingredient
+    ing3 = Faker::Food.ingredient
+    bread = @bread[rand(@bread.size)]
+    range = @range[rand(@range.size)]
     # define the Ruby Hash object for the document
     doc =
         {
-            :range => @range[rand(@range.size)],
-            :filling => "#{Faker::Food.ingredient}, #{Faker::Food.ingredient} and #{Faker::Food.ingredient}",
-            :on => @bread[rand(@bread.size)],
+            :description => "#{range} #{ing1},#{ing2} and #{ing3} on #{bread}",
+            :fillings => [
+                {
+                    :ingredient => ing1,
+                    :calories => rand(100),
+                },
+                {
+                    :ingredient => ing2,
+                    :calories => rand(100),
+                },
+                {
+                    :ingredient => ing3,
+                    :calories => rand(100),
+                },
+            ],
+            :on => bread,
             :price => @price[rand(@price.size)],
             :rating => rand(10),
             :lastUpdate => Time.now
